2d2236ef8dad191d8b7b2fae25bf9547177ff4a9
pi-arvis <ceo@nextfuturegenz.com>
2023-04-03 00:56:02 +0530

    setup lib raspi-servo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='raspi-servo',
+    packages=find_packages(include=['raspi-servo']),
+    version='0.1.0',
+    description='Intergation of servo motor with raspberry pi',
+    author='ashish',
+    license'MIT',
+)
